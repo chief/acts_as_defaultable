df0c9ae764094f399923ac9e05667ed29a91e18a
Bill Kolokithas <kolokithas.b@gmail.com>
2015-05-22 18:19:57 +0300

    Implement scope / relation feature

--- lib/acts_as_defaultable.rb
+++ lib/acts_as_defaultable.rb
@@ -8,12 +8,24 @@ module ActsAsDefaultable
 
       after_save :set_unique_default
 
-      column = options.first
+      if options.size > 1
+        column = options.first
+        relation = options.second[:relation]
+      elsif options.first.is_a?(Hash)
+        relation = options.first[:relation]
+      else
+        column = options.first
+      end
+
       column ||= :default
       column = column.to_s
-
+      relation = relation.to_s
+      relation_column = column + '_' + relation
 
       puts "acts_as_defaultable: Specify a column #{column} in #{self}" unless column_names.include?(column)
+      if !relation.blank? && !column_names.include?(relation_column)
+         puts "acts_as_defaultable: Specify a column #{relation_column} in #{self}"
+      end
 
       if column_names.include?(column)
         positive_value =
@@ -40,6 +52,10 @@ module ActsAsDefaultable
       class_methods = %(
         def self.default_column
           "#{column}"
+        end
+
+        def self.default_relation_column
+          "#{relation_column}"
         end
 
         def self.default_positive_value
@@ -69,6 +85,19 @@ module ActsAsDefaultable
               obj.update_attribute self.class.default_column, self.class.default_negative_value
             end
           end
+        end
+
+        def #{relation_column}
+         Object.const_get("#{relation.classify}").find_by_id(read_attribute(self.class.default_relation_column))
+        end
+
+        def #{relation_column}=(obj)
+         default_relation_id = obj.instance_of?(#{relation.classify}) ? obj.id : 0
+         write_attribute(self.class.default_relation_column, default_relation_id)
+        end
+
+        def after_destroy(record)
+          self.class.where(self.class.default_relation_column => record.id).update_all(self.class.default_relation_column => 0)
         end
 
       EOF
